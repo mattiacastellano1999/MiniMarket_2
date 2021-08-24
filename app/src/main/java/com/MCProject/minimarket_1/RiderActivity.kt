1d99a7621b4f3997fa389b124b6123b5f4ec4aef
MattiaCastellano1999 <20031813@studenti.uniupo.it>
2021-08-24 18:21:21 +0200

    Sistemato: - quando da Gestor cancello un prodotto esegue correttamente la gestione dell'acrivity lifecycle - quando aggiungo un nuovo prodotto fa il refresh corretto e lo aggiunge - impostato il layout dei rider.
    Manca:
    - la parte di messaggistica
    - l'intera parte dei rider
    
    Problemi:
    - da gestore la modifica del prodotto non gestisce bene l'activity lifecycle
    - negli user una volta selezionato il market, non riesco a vedere la descrizione del prodotto quando lo tappo

--- app/src/main/java/com/MCProject/minimarket_1/RiderActivity.kt
+++ app/src/main/java/com/MCProject/minimarket_1/RiderActivity.kt
@@ -1,10 +1,20 @@
 package com.MCProject.minimarket_1
 
+import android.annotation.SuppressLint
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
 class RiderActivity: AppCompatActivity() {
 
-    override fun onBackPressed() {
+    @SuppressLint("SetTextI18n")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.rider_activity_main)
+    }
+
+
+        override fun onBackPressed() {
         //super.onBackPressed()
     }
 }
